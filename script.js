ec2d8b5630eef6afa6a5d8bc5b26d97ef7576092
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-12 14:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const urls = [
+  'https://jsonplaceholder.typicode.com/posts/1',
+  'https://jsonplaceholder.typicode.com/posts/2',
+  'https://jsonplaceholder.typicode.com/posts/3'
+];
+
+for(let url of urls) {
+	const response = fetch(url)
+		.then((res) => res.json())
+		.then((data) => console.log(data))
+}
